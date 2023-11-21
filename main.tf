a24571da1cf66228e54b3b109b6be5c1259c4751
India <India@India-PC>
2023-11-21 22:26:34 +0530

    m

--- main.tf
+++ main.tf
@@ -61,6 +61,11 @@ variable "parameters" {
     { name = "dev.roboshop.rabbitmq.MQ_PASS", value = "roboshop123", type = "SecureString" },
     { name = "dev.roboshop.payment.AMQP_PASS", value = "roboshop123", type = "SecureString" },
     { name = "dev.roboshop.dispatch.AMQP_PASS", value = "roboshop123", type = "SecureString" },
+    
+    { name = "dev.roboshop.rds.master_username", value = "mysqlrdsadmin", type = "String" },
+    { name = "dev.roboshop.rds.master_password", value = "RoboShop1234", type = "SecureString" },
+    { name = "dev.roboshop.docdb.master_username", value = "docdbadmin", type = "String" },
+    { name = "dev.roboshop.docdb.master_password", value = "RoboShop1234", type = "SecureString" },
   ]
 }
 
